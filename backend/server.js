1330960706871a9cb326f15be8492532d6a0164f
Naveen Mishra <nvmkr12345@gmail.com>
2026-06-26 14:00:04 +0530

    Deployment Changes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const cors = require("cors")
+const path = require("path");
 
 dotenv.config();
 connectDB();
